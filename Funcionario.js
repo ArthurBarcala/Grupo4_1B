6764146e42f5c3eb81ca0d7d7a6d251a92de372f
Ismael Costa <ismaelcosta949@gmail.com>
2022-04-12 19:47:58 -0300

    main

--- Funcionario.js
+++ Funcionario.js
@@ -8,11 +8,17 @@ class Funcionario{
     numeroDependentes;
 
     /*criação do método construtor*/
-    constructor (CPF, nomeCompleto, depto, cargo, salarioBaseMensal, numeroDependentes){this.CPF = CPF;
+    constructor (CPF, nomeCompleto, depto, cargo, salarioBaseMensal, numeroDependentes){
+    this.CPF = CPF;
     this.nomeCompleto = nomeCompleto;
     this.depto = depto;
     this.cargo = cargo;
     this.salarioBaseMensal = salarioBaseMensal;
     this.numeroDependentes = numeroDependentes;}
    
+    reajusteSalarioBase(aumentoSalario){
+        return salarioBaseMensal += aumentoSalario/100 * salarioBaseMensal
+    }
+    
+    
 }
