ad8b0711523691fe2bfb48f50b344dcaede28601
Mariana Sibinelli <mariana.vieira@germinare.org.br>
2022-04-12 19:37:08 -0300

    main

--- Funcionario.js
+++ Funcionario.js
@@ -6,4 +6,13 @@ class Funcionario{
     cargo;
     salarioBaseMensal;
     numeroDependentes;
+
+    /*criação do método construtor*/
+    constructor (CPF, nomeCompleto, depto, cargo, salarioBaseMensal, numeroDependentes){this.CPF = CPF;
+    this.nomeCompleto = nomeCompleto;
+    this.depto = depto;
+    this.cargo = cargo;
+    this.salarioBaseMensal = salarioBaseMensal;
+    this.numeroDependentes = numeroDependentes;}
+   
 }
